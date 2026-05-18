99bf75425496a162fb107546bbecb4215074a9ac
Rajiv Bharadwaj <44103483+rbharadwaj9@users.noreply.github.com>
2026-05-18 15:05:59 +0200

    Updates launch file to make rviz optional File organization is also consistent with other nodes

--- launch/start.py
+++ launch/start.py
@@ -1,17 +1,42 @@
-from launch import LaunchDescription
-from launch_ros.actions import Node
+import os
 from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.conditions import IfCondition
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
 
 def generate_launch_description():
-    rviz_config=get_package_share_directory('hesai_ros_driver')+'/rviz/rviz2.rviz'
-    # yaml_config=get_package_share_directory('hesai_ros_driver')+'/config/config.yaml'
+    description_dir = get_package_share_directory('hesai_ros_driver')
+
+    rviz_arg = DeclareLaunchArgument(
+        'rviz',
+        default_value='false',
+        description='Launch RViz2 visualisation'
+    )
+
+    yaml_config = os.path.join(description_dir, 'config', 'config.yaml')
+    rviz_config = os.path.join(description_dir, 'rviz', 'rviz2.rviz')
+
+    driver_node = Node(
+        namespace='hesai_ros_driver',
+        package='hesai_ros_driver',
+        executable='hesai_ros_driver_node',
+        output='screen',
+        parameters=[{'config_path': yaml_config}]
+    )
+
+    # --- optional visualisation ---
+    rviz_node = Node(
+        namespace='rviz2',
+        package='rviz2',
+        executable='rviz2',
+        arguments=['-d', rviz_config],
+        condition=IfCondition(LaunchConfiguration('rviz')),
+    )
+
     return LaunchDescription([
-        Node(
-            namespace='hesai_ros_driver', 
-            package='hesai_ros_driver', 
-            executable='hesai_ros_driver_node', 
-            output='screen',
-            # parameters=[{'config_path': yaml_config}]
-        ),
-        Node(namespace='rviz2', package='rviz2', executable='rviz2', arguments=['-d',rviz_config])
+        rviz_arg,
+        driver_node,
+        rviz_node
     ])
